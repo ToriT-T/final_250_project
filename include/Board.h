9b05dfe0be93b3b3bfa3437bd5723ca657e46568
Victoria Crouthamel <victoriacrouthamel@gmail.com>
2022-04-18 23:28:46 -0700

    initial commit

---
+++ include/Board.h
@@ -0,0 +1,24 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include <string>
+
+using namespace std;
+
+class Board
+{
+    public:
+        Board();
+        virtual ~Board();
+        void createGameBoard();
+        void createOs();
+        void createXs();
+        void printGameBoard();
+        int* coordinateHelper(string row, string col);
+        void movePiece(int startX, int startY, int endX, int endY);
+        //string movement();
+
+    private:
+        string arr[18][18];
+};
+
+#endif // BOARD_H
